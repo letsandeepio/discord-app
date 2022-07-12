56c00853bd148afcc83105943b34e3f1d9eda8b9
Sandeep <sandeepchopra7@gmail.com>
2022-07-12 19:43:44 -0400

    add active server indicator

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -19,14 +19,27 @@ function MyApp({ Component, pageProps }: AppProps) {
       <div className='flex h-screen text-gray-100'>
         <div className='space-y-2 overflow-y-scroll bg-gray-900 p-3'>
           <Link href='/'>
-            <a
-              className={`${
-                router.pathname === '/'
-                  ? 'rounded-2xl bg-brand text-white'
-                  : 'rounded-3xl bg-gray-700 text-gray-100 hover:rounded-2xl hover:bg-brand hover:text-white'
-              } flex h-12 w-12 items-center justify-center  transition-all duration-200  `}
-            >
-              <DiscordLogo className='h-5 w-7' />
+            <a className='group relative block'>
+              <div className='absolute -left-3 flex h-full items-center'>
+                <div
+                  className={`${
+                    router.pathname === '/'
+                      ? 'h-10'
+                      : 'h-5 scale-0 opacity-0 group-hover:scale-100 group-hover:opacity-100'
+                  } w-1 origin-left  rounded-r bg-white  transition-all duration-200  `}
+                ></div>
+              </div>
+              <div className='group-active:translate-y-px'>
+                <div
+                  className={`${
+                    router.pathname === '/'
+                      ? 'rounded-2xl bg-brand text-white'
+                      : 'rounded-3xl bg-gray-700 text-gray-100 hover:rounded-2xl hover:bg-brand hover:text-white'
+                  } flex h-12 w-12 items-center justify-center  transition-all duration-200 `}
+                >
+                  <DiscordLogo className='h-5 w-7' />
+                </div>
+              </div>
             </a>
           </Link>
 
