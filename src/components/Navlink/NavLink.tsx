321e910c452531a90b6b37ab4091fe7bc9a646bf
Sandeep <sandeepchopra7@gmail.com>
2022-07-22 09:29:36 -0400

    apply active container

--- src/components/Navlink/NavLink.tsx
+++ src/components/Navlink/NavLink.tsx
@@ -5,18 +5,20 @@ import React from 'react';
 interface NavLinkProps {
   href: string;
   children?: React.ReactNode;
-  active: boolean;
+  active?: boolean;
 }
 
 const NavLink: React.FC<NavLinkProps> = ({ href, children, active }) => {
   const router = useRouter();
+  const isActive = active || router.asPath === href;
+
   return (
     <Link href={href}>
       <a className='group relative block'>
         <div className='absolute -left-3 flex h-full items-center'>
           <div
             className={`${
-              router.asPath === href
+              isActive
                 ? 'h-10'
                 : 'h-5 scale-0 opacity-0 group-hover:scale-100 group-hover:opacity-100'
             } w-1 origin-left  rounded-r bg-white  transition-all duration-200  `}
@@ -25,7 +27,7 @@ const NavLink: React.FC<NavLinkProps> = ({ href, children, active }) => {
         <div className='group-active:translate-y-px'>
           <div
             className={`${
-              router.asPath === href
+              isActive
                 ? 'rounded-2xl bg-brand text-white'
                 : 'rounded-3xl bg-gray-700 text-gray-100 hover:rounded-2xl hover:bg-brand hover:text-white'
             } flex h-12 w-12 items-center justify-center  overflow-hidden transition-all duration-200 `}
