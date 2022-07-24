29ec0b9dc81752a721beb304fa3b91518d534de5
Sandeep <sandeepchopra7@gmail.com>
2022-07-24 18:55:01 -0400

    fix: minor fixes

--- src/pages/servers/[sid]/channels/[cid].tsx
+++ src/pages/servers/[sid]/channels/[cid].tsx
@@ -199,7 +199,7 @@ const MessageWithUser = ({ message }: { message: any }) => {
 
 const Message = ({ message }: { message: any }) => {
   return (
-    <div className='mt-1 py-1 pl-4 pr-16 leading-[22px] hover:bg-gray-950/[.07]'>
+    <div className='py-0.5 pl-4 pr-16 leading-[22px] hover:bg-gray-950/[.07]'>
       <p className='pl-14 text-gray-100'>{message.text}</p>
     </div>
   );
